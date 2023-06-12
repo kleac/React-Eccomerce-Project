4c90d89dda2c5a03c4c9160efc8548a31e7a0fa1
Your Name <you@example.com>
2023-06-12 14:57:19 +0200

    Using Context

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.scss';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { UserProvider } from './contexts/user.context';
+
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <BrowserRouter>
-      <App />
+    <BrowserRouter>
+      <UserProvider>
+       <App />     
+      </UserProvider>
   </BrowserRouter>
-  
   </React.StrictMode>
 );
+//every component inside the userProvider have access to my context
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
