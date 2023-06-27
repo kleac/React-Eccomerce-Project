534b2afdc1a247abcbbbecca270faed429861ec5
Your Name <you@example.com>
2023-06-27 14:51:11 +0200

    Finilazing Auth Listener

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -8,8 +8,6 @@ import {
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
-import { UserContext } from '../../contexts/user.context';
-
 import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
@@ -22,8 +20,6 @@ const defaultFormFields = {
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
-
-  const {setCurrentUser} = userContext(UserContext)
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
@@ -42,8 +38,6 @@ const SignUpForm = () => {
         email,
         password
       );
-
-      setCurrentUser(user)
 
       await createUserDocumentFromAuth(user, { displayName });
       resetFormFields();
@@ -106,6 +100,6 @@ const SignUpForm = () => {
       </form>
     </div>
   );
-}
+};
 
 export default SignUpForm;
